fd6a51fa3c47a883593928d4cfd9223e5c8057f1
Fabio Silva <fsilva_1992@hotmail.com>
2019-11-09 18:38:55 -0100

    Added more questions

--- Server/src/org/academiadecodigo/thunderstructs/Game/QuestionDB.java
+++ Server/src/org/academiadecodigo/thunderstructs/Game/QuestionDB.java
@@ -2,29 +2,42 @@ package org.academiadecodigo.thunderstructs.Game;
 
 public class QuestionDB {
 
-    public static final int NUM_OF_QUESTIONS = 23;
-    public static final String[] QUESTIONS_DATA_BASE = new String[]{"Which of these is a pillar of OOP: #None #Retraction #Inflection #Reflection #None ",
+    public static final int NUM_OF_QUESTIONS = 35;
+    public static final String[] QUESTIONS_DATA_BASE = new String[]{"Which of these is a pillar of OOP: #Retraction #Inflection #Reflection #None #None ",
             "Which one of these should be preferred over Inheritance: #Polymorphism #Compositon #Polyformism #Strategy #Compositon ",
-            "Which of these famous MCs once said 'Tá fixe!': #Nuno #Sara #Jójó #Soraia #Jójó ",
-            "What is an object? #A variable #An instance of a class #A reference to an attribute #Your Cousin #An instance of a class ",
-            "Which of these is a purpose of Encapsulation: #Scalable code #Hard to maintain #Become a cool programmer #Protect the code from others #Protect the code from others ",
+            "Which of these famous MCs says 'Tá fixe!' a lot of times: #Nuno #Sara #Tojó #Jojó #Tojó ",
+            "What is an Object? #A variable #An instance of a class #A reference to an attribute #Your Cousin #An instance of a class ",
+            "Which of these is a purpose of Encapsulation: #Non Scalable code #Hard to maintain #Become a cool programmer #Hide complexity of the code from user #Hide complexity of the code from user ",
             "What word would you use to describe a man who does not have all his fingers in one hand? #An idiot #A programmer #A man without fingers #A normal person #A normal person ",
             "In OOP, new classes can be defined by extending existing classes. This is an example of: #Encapsulation #Inheritance #Aggregation #Interface #Inheritance ",
             "Which of the following has a method named flush()? #Input Stream #Output Stream #Reader Stream #Input Stream reader #Output Stream ",
-            "It takes 10 men and 8 hours to build a wall, how much time does it take with 5 men? #4 hours #None, because the wall was built already #20 hours #2 hours #None, because the wall was built already ",
-            "How restrictive is default compared to public, protected or private accessibility? #Less restrictive than public #More restrictive than private #No restrictions #More restrictive than protected, less than private #More restrictive than protected, less than private ",
+            "It takes 10 men and 8 hours to build a wall, how much time does it take to build the same wall with 5 men? #4 hours #None, because the wall is already built #20 hours #2 hours #None, because the wall is already built ",
+            "How restrictive is default acess modifier compared to public, protected or private accessibility? #Less restrictive than public #More restrictive than private #No restrictions #More restrictive than protected, less than private #More restrictive than protected, less than private ",
             "OOP Inheritance models which relationship? #Has-a #Is-a #Contains #Wants to be #Is-a relationship ",
             "Whats the meaning of life? #none #69 #127.0.0.1 #404 #69 ",
             "There are 8 man sitting on a couch, three legs break and 6 man leave. How many legs remain? #5 legs #3 legs #1 legs #9 legs #5 legs ",
-            "What are the four pillars of OOP? #Inheritance, Abstraction, Composition, Polymorphism #Abstraction, Polyphormism, Encapsulation, Heritance #Abstraction, Polymorphism , Encapsulation, Inheritance #Objects, Methods, Properties, Main #Abstraction, Polymorphism , Encapsulation, Inheritance ",
+            "What are the four pillars of OOP? #Abstraction, Polyphormism, Encapsulation, Heritance #Inheritance, Abstraction, Composition, Polymorphism #Abstraction, Polymorphism , Encapsulation, Inheritance #Objects, Methods, Properties, Main #Abstraction, Polymorphism , Encapsulation, Inheritance ",
             "Aggregation establishes between classes a: #Strong Relationship #Love Relationship #Weak Relationship #None of the above #Weak Relationship ",
             "Between Inheritance and Composition we should: #Use whatever when we want #Always favor Composition #Always favor Inheritance #Who cares? As long as it works #Always favor Composition ",
             "What's a Design Pattern? #Simple GFX method #Repeatable solution to a recurring problem #Powerpoint Template #Study of Design #Repeatable solution to a recurring problem ",
             "Whats a good practice for programmers? #Never care about readability #Program to an Interface #Program to an Implementation #Never ask for help #Program to an Interface ",
             "Are you a programmer? #Hell yes! #I wish #Trying to #Kind of all of the above #Hell yes! ",
-            "Design Patterns are divide in ___ types. Complete the sentence. #2 #3 #4 #None of the above #3 ",
+            "Design Patterns are divided in ___ types. Complete the sentence. #2 #3 #4 #None of the above #3 ",
             "What are the types of Design Patterns? #Creation, Structure, Behavior #Creational, Structural, Behavioral #Architectural, Religious, Structural #Creational, Structural, Architectural #Creational, Structural, Behavioral ",
             "In Lithuanian how do your say: I'm the King of Kaunas? #Aš esu Kauno Karalius #Aš esu Karalhus Kaunas #Aš esu Kaunas Karalhus #None of the above #Aš esu Kauno Karalius ",
-            "How is Padawan MC Rui Sousa Nickname? #Tojó #JoJo #Ruizim #Mr. Worldwide #Tojó "
+            "How is Padawan MC Rui Sousa Nickname? #Tojó #JoJo #Ruizim #Mr. Worldwide #Tojó ",
+            "What does JVM stands for? #Joint Virtual Mechanic #Java Viral Master #Java Virtual Machinima #Java Virtual Machine #Java Virtual Machine",
+            "Who is your best friend? #José #IntelliJ #Tójo #All the Mc's #IntelliJ ",
+            "How many types of variables Java has? #2 #3 #5 #How many you want #2 ",
+            "What are the types of variables you know in Java? #Primitive and Reference #Primitive and Objects #Strings, Primitives and Reference# You stupid, So many! #Primitive and Reference ",
+            "How many Primitive variables exist? #4 #6 #8 #7 #8 ",
+            "A float is represented with how many bytes? #4 #8 #2 #16 #4 ",
+            "A boolean is represented with how many bytes? #4 #8 #2 #That's too much #That's too much ",
+            "A char is represented with how many bytes? #4 #8 #2 #16 #2 ",
+            "A long is represented with how many bytes? #4 #8 #2 #16 #8 ",
+            "An int is represented with how many bytes? #4 #8 #2 #16 #4 ",
+            "A double is represented with how many bytes? #4 #8 #2 #16 #8 ",
+            "What is a String in Java? #A sequence of characters #An Object #None of the above #First two #First two "
+
     };
 }
