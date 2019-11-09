e2ec2f3fe7ba8e9d4d025eb79cd91cc04fdd5df2
unknown <filipe.aa1995@gmail.com>
2019-11-09 00:21:22 +0000

    added questions, changed the art of the Title

--- Client/src/org/academiadecodigo/thunderstructs/Messages.java
+++ Client/src/org/academiadecodigo/thunderstructs/Messages.java
@@ -3,12 +3,12 @@ package org.academiadecodigo.thunderstructs;
 public class Messages {
 
     public static final String REQUEST_NEXT_QUESTION = "/Next question";
-
+    public static final String BLUE = "\u001B[34m";
     public static final String BLACK_BACKGROUND = "\033[40m";  // BLACK
     public static final String ANSI_RESET = "\u001B[0m";
     public static final String ANSI_YELLOW = "\u001B[33m";
     public static final String YELLOW_BOLD = "\033[1;33m"; // YELLOW
-    public static final String BAR = "=======================================================================";
+    public static final String BAR = "==================================================================================================";
 
     public static final String SPACE = "\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n";
 
@@ -52,10 +52,11 @@ public class Messages {
             " '----------------' \n\n\n\n\n";
 
     public static final String WELCOME =
-            YELLOW_BOLD + "   __        _        _____       _                    _                 \n" +
-            "   \\ \\  ___ | |__     \\_   \\_ __ | |_  ___ _ ____   __(_) _____      __  \n" +
-            "    \\ \\/ _ \\| '_ \\     / /\\/ '_ \\| __|/ _ \\ '__\\ \\ / /| |/ _ \\ \\ /\\ / /  \n" +
-            " /\\_/ / (_) | |_) | /\\/ /_ | | | | |_|  __/ |   \\ V / | |  __/\\ V  V /   \n" +
-            " \\___/ \\___/|_.__/  \\____/ |_| |_|\\__|\\___|_|    \\_/  |_|\\___| \\_/\\_/    \n" +
-            "\n" + BAR + "\n" + ANSI_RESET + "\n Enter your host:";
+            YELLOW_BOLD +"\n\n\n"+ BAR + "\n" + BAR + BLUE +"\n\n" + "     ██╗ ██████╗ ██████╗     ██╗███╗   ██╗████████╗███████╗██████╗ ██╗   ██╗██╗███████╗██╗    ██╗    \n" +
+                    "     ██║██╔═══██╗██╔══██╗    ██║████╗  ██║╚══██╔══╝██╔════╝██╔══██╗██║   ██║██║██╔════╝██║    ██║    \n" +
+                    "     ██║██║   ██║██████╔╝    ██║██╔██╗ ██║   ██║   █████╗  ██████╔╝██║   ██║██║█████╗  ██║ █╗ ██║    \n" +
+                    "██   ██║██║   ██║██╔══██╗    ██║██║╚██╗██║   ██║   ██╔══╝  ██╔══██╗╚██╗ ██╔╝██║██╔══╝  ██║███╗██║    \n" +
+                    "╚█████╔╝╚██████╔╝██████╔╝    ██║██║ ╚████║   ██║   ███████╗██║  ██║ ╚████╔╝ ██║███████╗╚███╔███╔╝    \n" +
+                    " ╚════╝  ╚═════╝ ╚═════╝     ╚═╝╚═╝  ╚═══╝   ╚═╝   ╚══════╝╚═╝  ╚═╝  ╚═══╝  ╚═╝╚══════╝ ╚══╝╚══╝     \n" +
+                    "\n" + YELLOW_BOLD + BAR + "\n" + BAR + "\n" + ANSI_RESET + "\n Enter your host:";
 }
